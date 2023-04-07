b2c10d2815991ad8a633aaf5f6ef5131909c7305
levantiendat <levan040669@gmail.com>
2023-04-07 18:46:20 +0700

    a

--- Calendar/MainForm.cs
+++ Calendar/MainForm.cs
@@ -26,27 +26,30 @@ namespace Calendar
 			{
 				CalendarEntities db = new CalendarEntities();
 				var q = db.NUser.Where(p => p.name == txtuser.Text).Select(p => p.idUser).FirstOrDefault();
-				var s = db.Meeting.Where(p => (p.host == q ||checklist(p)))
+				var s = db.Meeting
 					.Select(p => new ShowDATA { ID = p.idMeeting, DETAIL = p.name, LOCATION = p.location, NGAYBATDAU = p.startTime, NGAYKETTHUC = p.endTime, REMINDERTIME = p.remind, HOST = p.NUser.name }); ;
 				List<ShowDATA> s1=new List<ShowDATA>();
 				foreach(var i in s)
 				{
 					if(i.NGAYBATDAU.Value.Date==dateTimePicker1.Value.Date || i.NGAYKETTHUC.Value.Date == dateTimePicker1.Value.Date)
 					{
-						s1.Add(i);
+						if (checklist(i))
+						{
+							s1.Add(i);
+						}
 					}
 				}
 				data.DataSource = s1.ToList();
 			}
 		}
-		private bool checklist(Meeting m)
+		private bool checklist(ShowDATA m)
 		{
 			CalendarEntities db = new CalendarEntities();
 			var q = db.NUser.Where(p => p.name == txtuser.Text).Select(p => p.idUser).FirstOrDefault();
 			var ds = db.User_Meeting.Where(p => p.idUser == q).Select(p => p.idMeeting).ToList();
 			foreach( var i in ds)
 			{
-				if (m.idMeeting == i)
+				if (m.ID == i)
 				{
 					return true;
 				}
